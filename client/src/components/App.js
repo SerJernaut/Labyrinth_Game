84024d5603c7080546f15553f7894d7b61d07ca9
SerJernaut <serjernaut@yandex.ru>
2020-07-22 19:58:43 +0300

    Added client functionality for refresh_sign_in.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
-import React, {lazy, Suspense} from 'react';
-import './App.scss'
+import React, {lazy, Suspense, useEffect} from 'react';
 import {Route} from "react-router-dom";
+import store from "../store";
+import {createAuthRequestAction} from "../actions/actionCreators";
+import CONSTANTS from "../constants";
 
 const SignUpPage = lazy(() => import('./pages/SignUpPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
@@ -8,8 +10,15 @@ const LoginPage = lazy(() => import('./pages/LoginPage'));
 
 function App () {
 
+    useEffect( () => {
+        const refreshToken = localStorage.getItem(CONSTANTS.REFRESH_TOKEN_KEY)
+        if (refreshToken) {
+            store.dispatch(createAuthRequestAction());
+        }
+    }, []);
 
-  return (
+
+    return (
       <Suspense fallback={ <div>Loading...</div> }>
         <Route  path={ '/sign_up' } component={ SignUpPage }/>
         <Route   path={ '/login' } component={ LoginPage }/>
--- client/src/sagas/authSaga.js
+++ client/src/sagas/authSaga.js
@@ -1,12 +1,13 @@
 import { put } from 'redux-saga/effects';
 import { createAuthErrorAction, createAuthSuccessAction } from '../actions/actionCreators';
-import { signInUser, signUpUser } from '../api/http/axios/authController/index';
+import {signInUser, signInUserByRefreshToken, signUpUser} from '../api/http/axios/authController/index';
 
 export function * authSaga (action ) {
   try {
 
-      const {data: {user}} = action.registration ? yield signUpUser(action.data)
+      const {data: {user}} = action.data ? action.isRegistration ? yield signUpUser(action.data)
                                                       : yield signInUser(action.data)
+                                        : yield signInUserByRefreshToken()
 
 
     yield put(createAuthSuccessAction(user));
