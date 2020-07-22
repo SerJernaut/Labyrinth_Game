a754f3a5451cbb5adb63136c3901df74deadcfb2
SerJernaut <serjernaut@yandex.ru>
2020-07-22 17:13:16 +0300

    Created TOKEN_KEY constants and REST_API_URL constant

---
+++ client/src/constants/index.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+    ACCESS_TOKEN_KEY: 'ACCESS_TOKEN_KEY',
+    REFRESH_TOKEN_KEY: 'REFRESH_TOKEN_KEY',
+    REST_API_URL: 'http://localhost:3000'
+}
+
+export default CONSTANTS;
+
