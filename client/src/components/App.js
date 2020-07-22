d3d4e00872c9ffdf7180463294096715b402e7e1
SerJernaut <serjernaut@yandex.ru>
2020-07-22 17:16:00 +0300

    Created auth empty pages and added lazy to App.js Routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
-import React, {lazy, Suspense, useEffect} from 'react';
+import React, {lazy, Suspense} from 'react';
 import './App.scss'
+import {Route} from "react-router-dom";
+
+const SignUpPage = lazy(() => import('./pages/SignUpPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
 
 
 function App () {
 
 
   return (
-    <Suspense fallback={ <div>Loading...</div> }>
-
-    </Suspense>
+      <Suspense fallback={ <div>Loading...</div> }>
+        <Route  path={ '/sign_up' } component={ SignUpPage }/>
+        <Route   path={ '/login' } component={ LoginPage }/>
+      </Suspense>
   );
 }
 
---
+++ client/src/components/pages/LoginPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const LoginPage = () => {
+
+  return (
+      <>
+        <Link to={ '/sign_up' }>Login</Link>
+    </>
+  );
+};
+
+export default LoginPage;
