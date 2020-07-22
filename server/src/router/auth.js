1f0761b318b9abf6db4f079fa5d3f1226bd1faf4
SerJernaut <serjernaut@yandex.ru>
2020-07-22 12:37:14 +0300

    Created auth router and 4 queries for authentication using having created controllers and middlewares.

---
+++ server/src/router/auth.js
@@ -0,0 +1,50 @@
+const {AUTHENTICATE_USER_SCHEMA} = require("../utils/validation");
+const {createValidationMW} = require("../middlewares/validation/createValidationMW");
+const authRouter = require('express').Router();
+const UserController = require('../controllers/userController/index');
+const tokensController = require('../controllers/tokensController/index');
+const sendUserAndTokenPair = require('../middlewares/authentication/sendUserAndTokenPair');
+const sendTokenPair = require('../middlewares/authentication/sendTokenPair');
+
+
+authRouter.post('/sign_up',
+    createValidationMW(AUTHENTICATE_USER_SCHEMA),
+    UserController.createUser,
+    tokensController.signRefreshTokenByUserId,
+    tokensController.createRefreshToken,
+    tokensController.signAccessToken,
+    sendUserAndTokenPair,
+    );
+
+
+authRouter.post('/login',
+    createValidationMW(AUTHENTICATE_USER_SCHEMA),
+    UserController.findUserByNickName,
+    UserController.checkIsPasswordRight,
+    tokensController.signRefreshTokenByUserId,
+    tokensController.createRefreshToken,
+    tokensController.signAccessToken,
+    sendUserAndTokenPair
+);
+
+authRouter.post(
+    '/refresh_tokens',
+    tokensController.verifyRefreshToken,
+    tokensController.findRefreshTokenWithUser,
+    tokensController.signRefreshTokenByRefreshTokenUserData,
+    tokensController.updateRefreshToken,
+    tokensController.signAccessToken,
+    sendTokenPair,
+);
+
+authRouter.post(
+    '/refresh_sign_in',
+    tokensController.verifyRefreshToken,
+    tokensController.findRefreshTokenWithUser,
+    tokensController.signRefreshTokenByRefreshTokenUserData,
+    tokensController.updateRefreshToken,
+    tokensController.signAccessToken,
+    sendUserAndTokenPair,
+);
+
+module.exports = authRouter;
