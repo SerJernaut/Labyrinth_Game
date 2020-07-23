978a99e6f1bf4dd440fc171284029b8d86c63634
SerJernaut <serjernaut@yandex.ru>
2020-07-23 12:17:51 +0300

    Changed BASE_URL at Axios config.

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,7 @@
 const CONSTANTS = {
     ACCESS_TOKEN_KEY: 'ACCESS_TOKEN_KEY',
     REFRESH_TOKEN_KEY: 'REFRESH_TOKEN_KEY',
-    REST_API_URL: 'http://localhost:3000'
+    REST_API_URL: 'http://localhost:3000/api/'
 }
 
 export default CONSTANTS;
