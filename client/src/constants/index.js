33b20298f090ed636ac3d2d7c6d2b08e985315ca
SerJernaut <serjernaut@yandex.ru>
2020-07-24 13:35:57 +0300

    Changed REST_API_URL constant value

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,7 @@
 const CONSTANTS = {
     ACCESS_TOKEN_KEY: 'ACCESS_TOKEN_KEY',
     REFRESH_TOKEN_KEY: 'REFRESH_TOKEN_KEY',
-    REST_API_URL: 'http://localhost:3000/api/'
+    REST_API_URL: 'http://localhost:3001/api/'
 }
 
 export default CONSTANTS;
