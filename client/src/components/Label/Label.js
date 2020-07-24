afd58fbf3a826eba8f1d5fb96adf1e4929030d2a
SerJernaut <serjernaut@yandex.ru>
2020-07-24 19:57:38 +0300

    Added label default styles, removed extra classes

--- client/src/components/Label/Label.js
+++ client/src/components/Label/Label.js
@@ -1,11 +1,12 @@
 import React      from 'react';
 import PropTypes  from 'prop-types';
 import classNames from 'classnames';
+import styles from './Label.module.sass';
 
 
 const Label = ({ className, ...rest }) => {
 
-    const classNameValue = classNames(className);
+    const classNameValue = classNames(styles.label, className);
 
     return (
         <label className={classNameValue} {...rest}/>
