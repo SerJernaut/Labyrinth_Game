fc9dec7699d2c7a3e711d53fd8646fce69b02087
SerJernaut <serjernaut@yandex.ru>
2020-07-31 14:03:25 +0300

    Changed validationSchema errorText and styled.

--- client/src/components/forms/validationSchemas/authSchemas/index.js
+++ client/src/components/forms/validationSchemas/authSchemas/index.js
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 
 const NICKNAME_NAME_PATTERN = /^[A-Z][a-zA-Z0-9]{1,16}$/;
-const PASSWORD_PATTERN =  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)[A-Za-z0-9_@#%!?\-^]{8,16}$/;
+const PASSWORD_PATTERN =  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-const NICKNAME_PATTERN_MESSAGE = 'Enter the valid nickname';
-const PASSWORD_PATTERN_MESSAGE = 'Enter the valid password';
+const NICKNAME_PATTERN_MESSAGE = 'Nickname must starts from latin capital letter, contains at least one lowercase letter and be no longer than 16 characters';
+const PASSWORD_PATTERN_MESSAGE = 'Password must contains minimum eight latin characters, at least one letter and one number';
 
 
 const nickName = {nickName: Yup.string().label('Nick Name').matches(NICKNAME_NAME_PATTERN, NICKNAME_PATTERN_MESSAGE).required()};
